5722ad680011f37f4a90876c666abd426f96becc
Alireza Haghighatkhah <ahaghigh@univ.yo.oulu.fi>
2020-09-08 17:37:22 +0300

    Updated namespace for ws

--- backend/src/main/java/com/example/backend/service/impl/soap/SaveMessageServiceEndpoint.java
+++ backend/src/main/java/com/example/backend/service/impl/soap/SaveMessageServiceEndpoint.java
@@ -15,7 +15,7 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class SaveMessageServiceEndpoint implements SaveMessageService {
 
-    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+    public static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
 
     @Autowired
     private MessageEntityRepository repository;
--- backend/src/main/java/com/example/backend/service/impl/soap/WebServiceConfig.java
+++ backend/src/main/java/com/example/backend/service/impl/soap/WebServiceConfig.java
@@ -28,7 +28,7 @@ public class WebServiceConfig extends WsConfigurerAdapter {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("MessagesPort");
         wsdl11Definition.setLocationUri("/ws");
-        wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
+        wsdl11Definition.setTargetNamespace(SaveMessageServiceEndpoint.NAMESPACE_URI);
         wsdl11Definition.setSchema(schema);
         return wsdl11Definition;
     }
